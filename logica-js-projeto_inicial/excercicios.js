14e1e439b68f7f4029d5a946bf7211a5f3b8c8df
dev-andersonsena <dev.fullstack.anderson@gmail.com>
2025-01-19 20:40:44 -0300

    Inicio formação G8-ONE

---
+++ logica-js-projeto_inicial/excercicios.js
@@ -0,0 +1,19 @@
+alert ('Boas vindas ao nosso site');
+
+let nome = 'lua';
+
+const idade = 25;
+const numeroDeVendas = 50;
+const saldoDisponivel = 1000;
+let mensagemDeErro = 'Erro preencha todos os campos'
+
+if (idade && numeroDeVendas && saldoDisponivel) {
+    alert(mensagemDeErro)
+}
+
+let nomeUsuario = prompt('Qual seu nome: ')
+let idadeUsuario = prompt('Qual sua idade: ')
+
+if (idadeUsuario >= 18){
+    alert('pode tirar a carteira de motorista')
+}
